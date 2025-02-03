f9ec53a14a4ad64d2b26180f0bf0692227c412ec
Vageesh004 <vageesh.smd@gmail.com>
2025-02-03 10:05:56 +0530

    first commit

---
+++ src/main/java/Mphasis_Training/Proj1/App.java
@@ -0,0 +1,13 @@
+package Mphasis_Training.Proj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
