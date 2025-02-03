4c919d028ce94777a178c2395f12c38c42614c6b
Vageesh004 <vageesh.smd@gmail.com>
2025-02-03 10:26:39 +0530

    second commit

--- src/main/java/Mphasis_Training/Proj1/App.java
+++ src/main/java/Mphasis_Training/Proj1/App.java
@@ -6,8 +6,15 @@ package Mphasis_Training.Proj1;
  */
 public class App 
 {
+	
+	public void method1() {
+		System.out.println("Adding a method");
+	}
+	
     public static void main( String[] args )
     {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
     }
 }
